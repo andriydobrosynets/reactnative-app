d0cdab000e31d6033dcbeea609341c812e2b8272
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-09-13 19:53:04 +0300

    Lesson 1 CW

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Users from "./components/Users";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <Users/>
+        <Text>jjhjhjj</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
---
+++ components/Users.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet} from 'react-native';
+import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {getUsers} from "../services/api.service";
+import User from "./User";
+
+const Users = () => {
+
+    let [users, setUsers] = useState();
+
+    useEffect(() => {
+        getUsers().then(value => setUsers([...value]))
+    }, []);
+
+    return (
+        <View style={[styles.base]}>
+            <FlatList
+                data={users}
+                renderItem={({item}) => <User item={item}/>}
+                keyExtractor={item => '' + item.id}
+            />
+        </View>
+    );
+};
+export default Users;
+
+let styles = StyleSheet.create({
+    base: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'red',
+        height: '100%'
+    }
+});
